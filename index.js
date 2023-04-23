a27a1f639dcee30b601c4c05f71de712ec5d003b
Maires Souza <trabalho.maires@gmail.com>
2023-04-23 13:20:24 -0300

    add index

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+app.listen('3000')
+
+// middleware
+app.use(express.json())
+
+app.route('/').get((req, res) => res.send(req.query.name))
+
+app.route('/').post((req, res) => res.send(req.body))
+
+app.route('/:parametro').get((req, res) => res.send(req.params.parametro))
